9b5c4a26f1da225e5148bee342ee2a18a115a84a
yavkotylev <yavkotylev@gmail.com>
2016-07-19 09:54:31 +0300

    ff

---
+++ https:/github.com/yavkotylev/tizenAppRep.git/lesson1/js/main.js
@@ -0,0 +1,62 @@
+var startTime;
+var checkTime;
+var imgH = "<img src=\"images/tizen_32.png\" id=\'image_X\' width=\"32\"/>"
+
+//Initialize function
+var init = function () {
+	// TODO:: Do your initialization job
+	console.log("init() called");
+
+	// add eventListener for tizenhwkey
+	document.addEventListener('tizenhwkey', function(e) {
+		if(e.keyName == "back") {
+			try {
+				tizen.application.getCurrentApplication().exit();
+			} catch (error) {
+				console.error("getCurrentApplication(): " + error.message);
+			}
+		}
+	});
+};
+// window.onload can work without <body onload="">
+window.onload = init;
+
+function startTime() {
+	var today = new Date();
+	var h = today.getHours();
+
+	var m = today.getMinutes();
+	var s = today.getSeconds();
+	m = checkTime(m);
+	s = checkTime(s);
+	document.getElementById('divbutton1').innerHTML="Current time: " + h + ":" + m + ":" + s;
+	var t = setTimeout(startTime, 250);
+}
+
+function deleteImage(){
+	
+	if (document.getElementById('imageTizen32').innerHTML === ""){
+		document.getElementById('imageTizen32').innerHTML = imgH;
+	}
+	else{
+		document.getElementById('imageTizen32').innerHTML = "";
+	}
+
+}
+
+function Inf(){
+	if (document.getElementById('inf').innerHTML === " "){
+		document.getElementById('inf').innerHTML="Screen Width: " + screen.width;
+	}
+	else{
+		document.getElementById('inf').innerHTML=" ";
+	}
+	
+}
+
+function checkTime(i) {
+	if (i < 10) {
+		i="0" + i;
+	}
+	return i;
+}
